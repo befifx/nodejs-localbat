103988042e51abe918e8e4a87841994614c0ddf3
negi <negi@example.com>
2022-10-15 11:56:20 +0900

    prettier追加、フォーマット

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import express from "express";
-import PropertiesReader from "properties-reader";
+import express from 'express';
+import PropertiesReader from 'properties-reader';
 
 const app: express.Express = express();
 app.use(express.json());
@@ -10,39 +10,39 @@ app.use(
 );
 
 app.listen(3000, () => {
-  console.log("Start on port 3000.");
+  console.log('Start on port 3000.');
 });
 
 // バッチ実行
-app.post("/execbat", (req: express.Request, res: express.Response) => {
+app.post('/execbat', (req: express.Request, res: express.Response) => {
   // ログ出力
   console.log(JSON.stringify(req.body));
 
   // iniファイル修正
-  modifyIni(req.body["path"]);
+  modifyIni(req.body['path']);
 
   // バッチ実行
   execBat();
 
   // 返却値
-  res.send("OK");
+  res.send('OK');
 });
 
 // ini書き換え
 const modifyIni = (path: string) => {
-  const iniPath = "./bat/sample.ini";
+  const iniPath = './bat/sample.ini';
 
   const props = PropertiesReader(iniPath);
 
-  console.log(props.get("hoge.fuga"));
-  props.set("hoge.fuga", path);
+  console.log(props.get('hoge.fuga'));
+  props.set('hoge.fuga', path);
 
   props.save(iniPath).then(
     (data) => {
-      console.log("ini modify success");
+      console.log('ini modify success');
     },
     (err) => {
-      console.log("ini modify error");
+      console.log('ini modify error');
       console.log(JSON.stringify(err));
     }
   );
@@ -50,15 +50,12 @@ const modifyIni = (path: string) => {
 
 // バッチ実行
 const execBat = () => {
-  const appPath = ".\\bat\\sample.bat";
-  const argument = "";
-  const cmd = appPath + " " + argument;
-  require("child_process").execSync(
-    cmd,
-    (err: string, stdout: string, stderr: string) => {
-      if (err) console.log("err:", err);
-      if (stdout) console.log("stdout:", stdout);
-      if (stderr) console.log("stderr:", stderr);
-    }
-  );
+  const appPath = '.\\bat\\sample.bat';
+  const argument = '';
+  const cmd = appPath + ' ' + argument;
+  require('child_process').execSync(cmd, (err: string, stdout: string, stderr: string) => {
+    if (err) console.log('err:', err);
+    if (stdout) console.log('stdout:', stdout);
+    if (stderr) console.log('stderr:', stderr);
+  });
 };
